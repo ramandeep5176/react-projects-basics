1b962ead2c73624d6439b25f4df63b1b77be15d7
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-09-26 09:02:48 -0400

    first commit

---
+++ 01-birthday-reminder/setup/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+function App() {
+
+const [people,setPeople]=useState(data)
+
+
+
+  return (
+    <main>
+      <section className="container">
+        <h3>0 birthdays today</h3>
+        <List />
+        <button className="btn" onClick={() => console.log("you clicked")}>
+          {" "}
+          clear all
+        </button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
