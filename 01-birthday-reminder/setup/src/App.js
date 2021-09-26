523b8f45910fc850da6d46006a8111a255dbe08f
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-09-26 09:10:35 -0400

    bdDone

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,17 +2,14 @@ import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
 function App() {
-
-const [people,setPeople]=useState(data)
-
-
+  const [people, setPeople] = useState(data);
 
   return (
     <main>
       <section className="container">
-        <h3>0 birthdays today</h3>
-        <List />
-        <button className="btn" onClick={() => console.log("you clicked")}>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button className="btn" onClick={() => setPeople([])}>
           {" "}
           clear all
         </button>
