ad5d7c1a76cddf32894ad935d09bb8bde70cc782
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-10-20 18:17:46 -0400

    modalBar

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -2,16 +2,14 @@ import React, { useContext } from "react";
 import { FaBars } from "react-icons/fa";
 
 const Home = () => {
-  return <main>
-<button className="sidebar-toggle">
-  <FaBars/>
-</button>
-<button className="btn">
-  show moadal
-</button>
-
-
-  </main>;
+  return (
+    <main>
+      <button className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button className="btn">show moadal</button>
+    </main>
+  );
 };
 
 export default Home;
