6b1eb157b8a34e5f98c7d473139236fbafad8039
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-12-06 18:52:13 -0500

    stripe

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -3,7 +3,7 @@ import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  return <h2></h2>
 }
 
 export default Navbar
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,11 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const Appcontext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  return <Appcontext.Provider>{children}</Appcontext.Provider>;
+};
