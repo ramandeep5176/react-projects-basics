2daf925012759d7c3c7d918d450fe041846f2714
ramandeep5176 <ramandeepgill5176@gmail.com>
2022-01-30 13:16:50 -0500

    redo

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -7,53 +7,37 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+  // const [isError, setIsError] = useState(false);
 
-  const removeTour = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id);
-    setTours(newTours);
-  };
-
-  const fetchTours = async () => {
+  const getTour = async () => {
     setLoading(true);
     try {
       const response = await fetch(url);
       const tours = await response.json();
       setLoading(false);
       setTours(tours);
+      // console.log(data);
     } catch (error) {
       setLoading(false);
       console.log(error);
     }
   };
+
   useEffect(() => {
-    fetchTours();
+    getTour();
   }, []);
 
   if (loading) {
     return (
-      <main>
+      <h3>
         <Loading />
-      </main>
-    );
-  }
-  if (tours.length === 0) {
-    return (
-      <main>
-        <div className="title">
-          <h2>no tours left</h2>
-          <button className="btn" onClick={fetchTours}>
-            load more tours
-          </button>
-        </div>
-      </main>
+      </h3>
     );
   }
 
   return (
     <>
-      <main>
-        <Tours tours={tours} removeTour={removeTour} />
-      </main>
+      <Tours tours={tours} />
     </>
   );
 }
--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,28 +1,22 @@
 import React, { useState } from "react";
 
-const Tour = ({ id, image, info, price, name, removeTour }) => {
+const Tour = ({ id, name, info, image, price }) => {
   const [readMore, setReadMore] = useState(false);
 
   return (
-    <article className="single-tour">
-      <img src={image} alt={name} />
-      <footer>
-        <div className="tour-info">
-          <h4>{name}</h4>
-          <h4 className="tour-price">${price} </h4>
-        </div>
-        <p>
-          {readMore ? info : `${info.substring(0, 200)}...`}
-          <button onClick={() => setReadMore(!readMore)}>
-            {readMore ? `show less` : `read more`}
-          </button>
-        </p>
-        <button onClick={() => removeTour(id)} className="delete-btn">
-          {" "}
-          not intrested
-        </button>
-      </footer>
-    </article>
+    <>
+      <article className="single-tour">
+        <img className="image" src={image} alt={name} />
+        <footer>
+          <div className="tour-info">
+            <h4>{name}</h4>
+            <h3 className="tour-price">{price}</h3>
+          </div>
+          <p>{info}</p>
+          <button className="delete-btn">delete</button>
+        </footer>
+      </article>
+    </>
   );
 };
 
